f9b04626ab7529898515098a7935b8ae150750d0
Daniel Guimarães <danguimaraes86@gmail.com>
2024-03-12 20:25:57 -0300

    [Cliente] add: ClienteControllerTest inicial

--- src/test/java/br/com/fiap/hackaton/fiaptrip/clientes/controllers/ClienteControllerTest.java
+++ src/test/java/br/com/fiap/hackaton/fiaptrip/clientes/controllers/ClienteControllerTest.java
@@ -2,58 +2,60 @@ package br.com.fiap.hackaton.fiaptrip.clientes.controllers;
 
 import br.com.fiap.hackaton.fiaptrip.clientes.models.Cliente;
 import br.com.fiap.hackaton.fiaptrip.clientes.services.ClienteService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import net.datafaker.Faker;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.Collections;
+import java.util.Locale;
+import java.util.Random;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest
 class ClienteControllerTest {
 
-    //    private static final Faker faker = new Faker(Locale.forLanguageTag("pt_BR"));
+    private static final Faker faker = new Faker(Locale.forLanguageTag("pt_BR"));
     AutoCloseable mock;
-    @Autowired
     private MockMvc mockMvc;
-    @MockBean
+    @Mock
     private ClienteService clienteService;
 
-//    private static Cliente getClienteMock() {
-//        return new Cliente(
-//                new Random().nextLong(),
-//                faker.witcher().witcher(),
-//                faker.country().countryCode3(),
-//                faker.date().birthdayLocalDate(),
-//                faker.cpf().valid(),
-//                faker.passport().valid(),
-//                faker.phoneNumber().phoneNumber(),
-//                faker.internet().emailAddress(),
-//                faker.address().fullAddress()
-//        );
-//    }
+    private static Cliente getClienteMock() {
+        return new Cliente(
+                new Random().nextLong(),
+                faker.witcher().witcher(),
+                faker.country().countryCode3(),
+                faker.date().birthdayLocalDate(),
+                faker.cpf().valid(),
+                faker.passport().valid(),
+                faker.phoneNumber().phoneNumber(),
+                faker.internet().emailAddress(),
+                faker.address().fullAddress()
+        );
+    }
 
-//    public static String convertToJson(Object object) throws JsonProcessingException {
-//        return new ObjectMapper()
-//                .registerModule(new JavaTimeModule())
-//                .writeValueAsString(object);
-//    }
+    public static String convertToJson(Object object) throws JsonProcessingException {
+        return new ObjectMapper()
+                .registerModule(new JavaTimeModule())
+                .writeValueAsString(object);
+    }
 
     @BeforeEach
     void setupUp() {
@@ -76,12 +78,12 @@ class ClienteControllerTest {
         @Test
         void deveRetornarListaVazia() throws Exception {
             Page<Cliente> clientesMock = new PageImpl<>(Collections.emptyList());
-            when(clienteService.findAllClientes(PageRequest.ofSize(10)))
+            when(clienteService.findAllClientes(any(Pageable.class)))
                     .thenReturn(clientesMock);
 
-            mockMvc.perform(get("/clientes").param("size", "10"))
-                    .andExpect(status().isOk());
-//                    .andExpect(content().json(convertToJson(clientesMock)));
+            mockMvc.perform(get("/clientes"))
+                    .andExpect(status().isOk())
+                    .andExpect(content().json(convertToJson(clientesMock)));
             verify(clienteService, times(1))
                     .findAllClientes(any(Pageable.class));
         }
