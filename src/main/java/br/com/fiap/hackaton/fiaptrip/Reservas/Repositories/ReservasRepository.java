ca809681f12dea0feda3dda67a2fb855b9b63125
Felipe Ferreira de Paula <felipefp2@gmail.com>
2024-03-12 20:23:39 -0300

    adicionando reserva

---
+++ src/main/java/br/com/fiap/hackaton/fiaptrip/Reservas/Repositories/ReservasRepository.java
@@ -0,0 +1,4 @@
+package br.com.fiap.hackaton.fiaptrip.Reservas.Repositories;
+
+public interface ReservasRepository {
+}
---
+++ src/main/java/br/com/fiap/hackaton/fiaptrip/Reservas/Services/ReservaService.java
@@ -0,0 +1,4 @@
+package br.com.fiap.hackaton.fiaptrip.Reservas.Services;
+
+public class ReservaService {
+}
