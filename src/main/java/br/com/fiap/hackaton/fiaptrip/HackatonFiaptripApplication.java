ceaf720f11c7c87dcfe8d54fe674a80ee5a626a5
Daniel Guimarães <danguimaraes86@gmail.com>
2024-03-11 21:06:27 -0300

    add: commit inicial

---
+++ src/main/java/br/com/fiap/hackaton/fiaptrip/HackatonFiaptripApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.hackaton.fiaptrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatonFiaptripApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackatonFiaptripApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/fiap/hackaton/fiaptrip/HackatonFiaptripApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.hackaton.fiaptrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackatonFiaptripApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
