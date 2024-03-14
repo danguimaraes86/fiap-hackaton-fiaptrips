973d51bdffad772fdc3b8b878dd239f6ec25dee8
Daniel Guimarães <danguimaraes86@gmail.com>
2024-03-14 20:05:42 -0300

    chore: remover imports

--- src/main/java/br/com/fiap/hackaton/fiaptrip/quartos/models/Localidade.java
+++ src/main/java/br/com/fiap/hackaton/fiaptrip/quartos/models/Localidade.java
@@ -4,9 +4,10 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
-import lombok.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import java.util.List;
 import java.util.Set;
 
 @Getter
